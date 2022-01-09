535bf64311bc955aa32f22b15b5aaa4c2bbabe9d
r0bomurlok <igrtnsk@ya.ru>
2022-01-09 18:17:44 +0300

    fix: Поправить стили [09/01 18:17]

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,13 @@ import React from "react";
 
 function Card({card, onCardClick}) {
     return (
-        <li className="card" onClick={() => {
-            onCardClick(card);
-        }}>
+        <li className="card">
         <button type="button" className="card__delete-button button"></button>
-        <img className="card__img" src={card.link} alt={card.name} />
+        <img className="card__img" src={card.link} alt={card.name}  
+        onClick={() => {
+            onCardClick(card);
+        }} 
+        />
         <div className="card__items">
           <h2 className="card__capture">{card.name}</h2>
           <button type="button" className="card__like-button button"></button>
